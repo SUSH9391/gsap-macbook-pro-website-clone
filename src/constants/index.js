b43374b9963c335c2a938d40dde5c3c9dc08451a
Sushmitha Baliga B <masrbaliga@gmail.com>
2025-11-02 12:43:09 +0530

    Navbar.jsx added

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+export const navlinks = [
+    
+            { label: "Store" },
+            { label: "Mac" },
+            { label: "iPhone" },
+            { label: "Watch" },
+            { label: "Vision" },
+            { label: "AirPods" },
+          
+]
